3522c4dc64e4e6bce2a5549c7e87f25b822a07e1
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-08-12 21:42:17 +0200

    feat: add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    rules: {
+      eqeqeq: "error",
+      "no-unused-vars": "warn",
+      "no-unused-vars": "error",
+      "no-var": "error",
+      
+    },
+    files: ["*.js"],
+  },
+];
