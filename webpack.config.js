5cb2c58ecd219cfb4406e587a08d674fbe5dc34d
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-08-12 21:27:14 +0200

    add ESLint options

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,21 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new ESLintPlugin({
+      cache: true,
+      cacheLocation: path.resolve(__dirname, "node_modules/.cache/eslint-webpack-plugin/.eslintcache"),
+      overrideConfigFile: path.resolve(__dirname, "eslint.config.js"),
+      exclude: 'node_modules',
+      extensions: ['js'],
+      fix: false,
+      formatter: 'stylish',
+      lintDirtyModulesOnly: false,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: false,
+      quiet: false,
+    }),
+  ],
 };
